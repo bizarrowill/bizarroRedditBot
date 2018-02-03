9a7adf16c8dd866eeb19b85b62771bfa790830de
William Robertson <williamp.robertson@gmail.com>
2018-02-03 09:59:14 -0700

    initial framework

---
+++ app.js
@@ -0,0 +1,31 @@
+//        Global Variables
+require("dotenv").config();
+
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+
+// Build Snoowrap and Snoostorm clients for API calls
+const r = new Snoowrap({
+    userAgent: 'reddit-bot-example-node',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(r);
+
+// Configure options for stream: subreddit & results per query
+const streamOpts = {
+    subreddit: 'all',
+    results: 25
+};
+  
+
+// Create a Snoostorm CommentStream with the specified options
+const comments = client.CommentStream(streamOpts);
+
+// On comment, perform whatever logic you want to do
+comments.on('comment', (comment) => {
+    console.log(comment);
+});
